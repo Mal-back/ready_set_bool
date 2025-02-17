1b1b2fffb2a9672ab4a37ff6de132036c5448ba4
Valentin Levy <purmerinos@protonmail.com>
2025-02-17 14:09:40 +0100

    Truth table working with subject example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ready_set_bool::ready_set_bool::truth_table::TruthTable;
 
 fn main() {
-    let tree = TruthTable::build_from_str("11&");
-    println!("{tree:?}");
+    let tree = TruthTable::build_from_str("AB&C|").unwrap();
+    tree.compute_truth_table();
 }
--- src/ready_set_bool/truth_table.rs
+++ src/ready_set_bool/truth_table.rs
@@ -1,4 +1,4 @@
-use std::collections::hash_map;
+use std::collections::{hash_map, HashMap};
 
 use crate::ready_set_bool::Error;
 
@@ -68,7 +68,57 @@ impl TruthTable {
         let number_of_variables = self.expression_variables.len();
         let number_of_combination = 2u32.pow(number_of_variables as u32);
 
-        for variable_values in 0..number_of_combination {}
+        self.print_truth_table_header();
+
+        for variables_values in 0..number_of_combination {
+            let value_map = self.compute_values_map(&variables_values);
+            let result = self.root.resolve_node(&value_map);
+            self.print_truth_table_line(&value_map, result);
+        }
+    }
+
+    fn compute_values_map(&self, variables_values: &u32) -> HashMap<&char, bool> {
+        let mut current_map = HashMap::new();
+        for (index, variable) in self.expression_variables.iter().enumerate() {
+            let variable_value = self.get_concrete_value(variables_values, index);
+            current_map.insert(variable, variable_value);
+        }
+        current_map
+    }
+
+    fn get_concrete_value(&self, variables_values: &u32, index: usize) -> bool {
+        let mask = 1 << index;
+
+        let bit_of_interest = variables_values & mask;
+
+        if bit_of_interest != 0 {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn print_truth_table_header(&self) {
+        for variable in self.expression_variables.iter().rev() {
+            print!("| {variable} ");
+        }
+        println!("| = |");
+        for _ in 0..self.expression_variables.len() {
+            print!("|---")
+        }
+        println!("|---|");
+    }
+
+    fn print_truth_table_line(&self, value_map: &HashMap<&char, bool>, result: bool) {
+        for variable in self.expression_variables.iter().rev() {
+            print!(
+                "| {} ",
+                *value_map
+                    .get(variable)
+                    .expect("Variable should be present in the map") as u32
+            );
+        }
+        println!("| {} |", result as u32);
     }
 }
 
@@ -100,48 +150,37 @@ impl TreeNode {
         })
     }
 
-    pub fn resolve_node(&self, variables_values: u32, expression_variables: Vec<char>) -> bool {
-        match self.node_type {
+    pub fn resolve_node(&self, value_map: &HashMap<&char, bool>) -> bool {
+        let operation_closure = match &self.node_type {
             NodeType::Leaf(_) => panic!("resolve node should never be call on a leaf"),
-            NodeType::Node(_) => {}
+            NodeType::Node(operation) => operation.get_operation_closure(),
+        };
+        let left_value;
+        let right_value;
+        if let Some(left_child) = &self.left_child {
+            left_value = match left_child.node_type {
+                NodeType::Leaf(var) => *value_map
+                    .get(&var)
+                    .expect("All variables should be in the map"),
+                NodeType::Node(_) => left_child.resolve_node(value_map),
+            }
+        } else {
+            panic!("A left_child should be present at this point");
         }
-        match self.left_child
-        todo!()
-    }
-
-    fn get_leaf_value(&self, variables_values: u32, expression_variables: Vec<char>) -> bool {
-        match self.node_type {
-            NodeType::Node(_) => panic!("This should only be called on a leaf"),
-            NodeType::Leaf(variable) => {
-                self.get_concrete_value(variable, variables_values, expression_variables)
+        if let Some(right_child) = &self.right_child {
+            right_value = match right_child.node_type {
+                NodeType::Leaf(var) => *value_map
+                    .get(&var)
+                    .expect("All variables should be in the map"),
+                NodeType::Node(_) => right_child.resolve_node(value_map),
+            }
+        } else {
+            right_value = match self.node_type {
+                NodeType::Node(Operation::Not) => false,
+                _ => panic!("A right_child should be present at this point"),
             }
         }
-    }
-
-    fn get_concrete_value(
-        &self,
-        variable: char,
-        variables_values: u32,
-        expression_variables: Vec<char>,
-    ) -> bool {
-        let variable_index = expression_variables
-            .iter()
-            .position(|c| *c == variable)
-            .expect("Variable should be present in the known variables");
-
-        self.extract_current_value(variable_index + 1, variables_values)
-    }
-
-    fn extract_current_value(&self, index: usize, variables_values: u32) -> bool {
-        let mask = 1 << index;
-
-        let bit_of_interest = variables_values & mask;
-
-        if bit_of_interest != 0 {
-            true
-        } else {
-            false
-        }
+        operation_closure(left_value, right_value)
     }
 }
 
