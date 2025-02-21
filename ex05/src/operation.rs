0905e93543c25cff94821101f7143cd6421e1ccc
Valentin Levy <purmerinos@protonmail.com>
2025-02-21 16:19:44 +0100

    Remove useless operation method

--- ex05/src/operation.rs
+++ ex05/src/operation.rs
@@ -22,22 +22,6 @@ impl Operation {
             _ => panic!("Unknown Symbol should have been seen earlier"),
         }
     }
-    pub fn get_operation_closure(&self) -> impl FnOnce(bool, bool) -> bool {
-        match self {
-            Operation::Equality => |first, second| first == second,
-            Operation::Not => |first: bool, _| !first,
-            Operation::And => |first, second| first & second,
-            Operation::Or => |first, second| first | second,
-            Operation::Xor => |first, second| first ^ second,
-            Operation::IfThen => |first: bool, second| {
-                if !first || (first && second) {
-                    true
-                } else {
-                    false
-                }
-            },
-        }
-    }
 }
 
 impl Display for Operation {
